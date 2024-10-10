e8350babdd31840623e8ec43aafc2f2d1b983e1a
theshanumalik <shanumalik9001567@gmail.com>
2024-10-10 11:28:01 +0530

    this keyword and classes

--- js/object.js
+++ js/object.js
@@ -61,16 +61,63 @@
 
 // newObj.say("Hello world");
 
-const print = {
-  text: "Some text",
-  setText: function (input) {
-    this.text = input;
-  },
-  exec: function () {
-    console.log(this.text);
-  },
-};
+// const print = {
+//   text: "Some text",
+//   setText: function (input) {
+//     this.text = input;
+//   },
+//   exec: function () {
+//     console.log(this.text);
+//   },
+// };
 
-print.setText("override");
+// print.setText("override");
 
-print.exec();
+// print.exec();
+
+// console.log(this);
+
+// const student = {
+//   firstname: "Manish",
+//   lastname: "sharma",
+//   fullName: student.firstname + student.lastname,
+//   // fullName: function () {
+//   //   return firstname + " " + lastname;
+//   // },
+// };
+
+// console.log(student.fullName());
+
+// class Student {
+//   constructor(a, b, c) {
+//     this.name = a;
+//     this.age = b;
+//     this.course = c;
+//     this.fullName = this.name + " Sharma";
+//   }
+
+//   generateMarksheet() {
+//     console.log("_____");
+//     console.log(this.name);
+//     console.log("_____");
+//   }
+// }
+
+// const manish = new Student("manish", 20, "BCA");
+// const shanu = new Student("shanu", 20, "BCA");
+
+// console.log(manish, shanu);
+
+// manish.generateMarksheet();
+
+// const str = "ali";
+
+// console.log(manish["full-Name"]);
+
+// const obj = {
+//   "full-name": "ali chauhan",
+// };
+
+// console.log(obj["full-name"]);
+
+// isAdmin
